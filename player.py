25f53ba3e65b41cf8e3fb35b8272e064061bc4f3
fatih <rinqtf@gmail.com>
2026-09-12 01:06:38 +0300

    feat: add player movement controls
    Add PLAYER_SPEED and a Player.move() method that moves the ship in
    its facing direction. Support W/S keyboard input to accelerate and
    reverse, using the ship's rotation to compute movement direction.

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
@@ -11,6 +11,12 @@ class Player(CircleShape):
 
     def rotate(self, dt: float) -> None:
         self.rotation += dt * PLAYER_TURN_SPEED
+
+    def move(self, dt: float) -> None:
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
 
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -30,3 +36,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
