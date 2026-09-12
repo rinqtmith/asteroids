e3dddc0635b6c811e5cdea4e57bafc582e702857
fatih <rinqtf@gmail.com>
2026-09-12 13:59:08 +0300

    feat: add collision handling
    Add circle collision detection to asteroid sprites and detect player
    collisions in the main loop. Log the player hit event and exit the
    game when a collision occurs.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
+
 import pygame
 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
@@ -20,7 +22,7 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
 
-    _ = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     _ = AsteroidField()
     dt = 0.0
 
@@ -32,6 +34,13 @@ def main():
                 return
 
         updatable.update(dt)
+        for asteroid in asteroids:
+            asteroid: Asteroid
+
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
