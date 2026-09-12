b4012820f20cd21def1dac06d1c4243450dd2780
fatih <rinqtf@gmail.com>
2026-09-12 14:44:05 +0300

    feat: add asteroid splitting
    When an asteroid is shot, split it into two smaller asteroids with
    rotated velocities instead of destroying it outright. Asteroids below
    minimum radius are destroyed. Log the split event.

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -13,3 +16,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+
+    def split(self) -> None:
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+
+        angle = random.uniform(20, 50)
+        first = self.velocity.rotate(angle)
+        second = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = first * 1.2
+        asteroid2.velocity = second * 1.2
